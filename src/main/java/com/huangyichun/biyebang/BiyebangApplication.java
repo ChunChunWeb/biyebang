1c9549a3ace537a45158c51ac92dddd09c83bc52
Hadi\ba <499154897@qq.com>
2019-08-15 11:23:00 +0800

    初始化springboot项目 添加组件。

---
+++ src/main/java/com/huangyichun/biyebang/BiyebangApplication.java
@@ -0,0 +1,13 @@
+package com.huangyichun.biyebang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiyebangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiyebangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huangyichun/biyebang/BiyebangApplicationTests.java
@@ -0,0 +1,16 @@
+package com.huangyichun.biyebang;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BiyebangApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
