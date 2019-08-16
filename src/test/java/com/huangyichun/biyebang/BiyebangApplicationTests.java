de648a9fc3359718fb5fd4c525458973568f936b
Hadi\ba <499154897@qq.com>
2019-08-16 20:38:48 +0800

    添加html模板

--- src/test/java/com/huangyichun/biyebang/BiyebangApplicationTests.java
+++ src/test/java/com/huangyichun/biyebang/BiyebangApplicationTests.java
@@ -5,12 +5,16 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BiyebangApplicationTests {
 
     @Test
     public void contextLoads() {
+
     }
 
 }
